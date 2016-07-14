3b6f984b4a97b6d2ad7e6d1958de75ab30250ba5
Palina K <p-kalenik@ya.ru>
2016-07-14 19:04:36 +0400

    +

--- app.js
+++ app.js
@@ -8,30 +8,46 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 var tasks = [
-  {name : 'wake up', checkboxState: true},
-  {name : 'survive', checkboxState: false},
-  {name : 'back to sleep', checkboxState: false}
+  {name : 'wake up', checkboxState: true, id : 0},
+  {name : 'survive', checkboxState: false, id : 1},
+  {name : 'back to sleep', checkboxState: false, id : 2}
   ];
 
+var id = 2;
 app.get('/tasks', function (req, res) {
   res.send(tasks);
 })
 
 app.post('/addTask', function (req, res) {
-  tasks.push(req.body);
-  res.sendStatus(200)
+  if(typeof req.body.name === 'string'){
+    id++
+    var task = { name : req.body.name,
+            checkboxState : false,
+            id : id};
+    tasks.push(task);
+    res.send(task);
+  }else{
+    console.log('error');
+  }
 })
 
 app.post('/removeTask', function (req, res) {
-  tasks.splice(req.body.itemIndex, 1)
-  res.sendStatus(200)
+  for (var i = tasks.length - 1; i >= 0; i--) {
+    if(tasks[i].id === req.body.id){
+      tasks.splice(i, 1);
+      res.send(tasks);
+    }
+  }
 })
 
-app.post('/changeChecboxState', function (req, res) {
-  for (id in tasks) {
-    if (tasks[id].name == req.body.name) tasks[id].checkboxState = !tasks[id].checkboxState
-  }
-  res.sendStatus(200)
+app.post('/changeItemState', function (req, res) {
+  tasks.forEach(function(item, i, arr) {
+    if(item.id === req.body.id){
+      tasks[i].checkboxState = !tasks[i].checkboxState
+        res.sendStatus(200);
+    }
+  })
+
 })
 
 app.get('/removeChecked', function (req, res) {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,10 +9,9 @@ app.controller('mainCtrl', function ($http, $scope) {
 		})
 	$scope.addTask = function (task) {
 		if(task){
-			task.checkboxState = false;
 			$http.post('http://localhost:3000/addTask', task)
 				.success(function (result) {
-					$scope.tasks.push(task);
+					$scope.tasks.push(result);
 					$scope.task = null;
 				})
 				.error(function (result) {
@@ -20,19 +19,17 @@ app.controller('mainCtrl', function ($http, $scope) {
 				})
 		}
 	}
-	$scope.removeTask = function (item) {
-		console.log($scope.tasks)
-		var itemIndex = $scope.tasks.indexOf(item);
-		$http.post('http://localhost:3000/removeTask', {'itemIndex' : itemIndex})
+	$scope.removeTask = function (id) {
+		$http.post('http://localhost:3000/removeTask', {'id' : id})
 			.success(function (result) {
-				$scope.tasks.splice(itemIndex, 1);
+				$scope.tasks = result;
 			})
 			.error(function (result) {
 				console.log('error in post');
 			})
   	}
-  	$scope.changeChecboxState = function (item) {
-  		$http.post('http://localhost:3000/changeChecboxState', item)
+  	$scope.changeChecboxState = function (id) {
+  		$http.post('http://localhost:3000/changeItemState', {'id' : id})
 			.success(function (result) {
 				console.log('Checbox chenged', result);
 			})
